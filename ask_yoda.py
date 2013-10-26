32e208cc6c35d5dd34a14d1f5d7353b3d569d04b
Alex Lynham <acelynham@hotmail.com>
2013-10-26 14:54:55 +0100

    Changed index, added receive-sms route for clockwork integration.

--- ask_yoda.py
+++ ask_yoda.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+  return 'Welcome to Ask Yoda. Seem to be using a browser you are. Text us, you must.'
+
+@app.route('/receive-sms')
 def hello_world():
   return 'Hello World!'
 
