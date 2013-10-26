e12936e32701d9a5aaa1aaf24bb6f73913bf8040
Alex Lynham <acelynham@hotmail.com>
2013-10-26 14:48:44 +0100

    Removed explicit format, still port 80

--- ask_yoda.py
+++ ask_yoda.py
@@ -2,10 +2,9 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
-  if request.method == 'GET':
-    return 'Hello World!'
+  return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(port=80)
