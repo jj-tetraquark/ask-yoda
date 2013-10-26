d0f7632a7d784ada183d9f69854054fc6930bbad
Alex Lynham <acelynham@hotmail.com>
2013-10-26 16:04:28 +0100

    Question class and logging sorted, ready to test.

--- ask_yoda.py
+++ ask_yoda.py
@@ -2,6 +2,8 @@ import os
 import re
 import question
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,8 +11,13 @@ def index():
   return 'Welcome to Ask Yoda. Seem to be using a browser you are. Text us, you must.'
 
 @app.route('/receive-sms')
-def hello_world():
-  return 'Hello World!'
+def accept_input():
+  question = Question.new(
+    request.args.get('content', ''),
+    request.args.get('from', '')
+    )
+  print question.message
+  print question.number
 
 if __name__ == '__main__':
     app.run(port=80)
